645bcb38ea3581af0b4bd54e8bf70327ab023ee8
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-06 16:03:13 +0300

    Add update_posts_counter in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, foreign_key: :post_id
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
