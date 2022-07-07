828f16cfe702256e32c92e9e73c751376eab0e87
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 12:36:13 +0300

    Add validation for user name and user post_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
+  validates :name, presence: true, length: { in: 3..10 }
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_three_posts
     posts.includes(:author).order(created_at: :DESC).limit(3)
   end
