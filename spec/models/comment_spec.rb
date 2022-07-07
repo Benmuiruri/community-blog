75a9c982f5bcdd0f606a456e0edcdcb1d7c9f03a
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 16:30:58 +0300

    Add comment text validation and tests for comment validity

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
+  let(:post) do
+    Post.create(author: user1, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+  end
+  let(:comment1) { Comment.create(post:, author: user1, text: 'This is my first comment!') }
+
+  context 'comment' do
+    it 'cannot be blank' do
+      comment1.text = nil
+      expect(comment1).to_not be_valid
+    end
+
+    it 'is valid if length is 3 characters or more' do
+      expect(comment1).to be_valid
+    end
+  end
 end
