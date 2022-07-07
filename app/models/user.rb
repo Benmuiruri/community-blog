f3b6831f5edd43bac15c8b31d6d29c5518c0d6a0
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 12:18:59 +0300

    Regenerate models to create the associated spec files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,2 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-
-  def last_three_posts
-    posts.includes(:author).order(created_at: :DESC).limit(3)
-  end
 end
