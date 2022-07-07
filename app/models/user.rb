49911d4d9bce19226f5646b961be71693a557e2c
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 13:31:09 +0300

    Add dependent: :destroy in user and post to allow cascading delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+  has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   validates :name, presence: true, length: { in: 3..10 }
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
