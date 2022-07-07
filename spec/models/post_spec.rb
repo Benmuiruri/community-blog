0b65a7c4adce7b09a75a85bc66af6ff701b48828
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 14:44:35 +0300

    Add tests for post likes_counter

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let (:user) { User.create(name: 'Monica', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
-  let (:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+  let(:user) { User.create(name: 'Monica', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0, likes_counter: 0) }
+  let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
 
-   context 'post.title' do
+  context 'post.title' do
     it 'is not valid with a name less than 3 characters' do
       post.title = 'M'
       expect(post).to_not be_valid
@@ -22,6 +22,17 @@ RSpec.describe Post, type: :model do
     end
 
     it 'is valid if comments_counter equal or greater than 0' do
+      expect(post).to be_valid
+    end
+  end
+
+  context 'posts.likes_counter' do
+    it 'is not valid if likes_counter is not equal or greater than 0' do
+      post.likes_counter = nil
+      expect(post).to_not be_valid
+    end
+
+    it 'is valid if likes_counter equal or greater than 0' do
       expect(post).to be_valid
     end
   end
