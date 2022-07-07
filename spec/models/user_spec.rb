dc19b261c2e684f7625a1c08caadcace8a87fd03
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 15:25:15 +0300

    Fix Rubocop offenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  let (:user) { User.create(name: 'Monica', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
+  let(:user) { User.create(name: 'Monica', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
 
   context '.name' do
     it 'is not valid with a name less than 3 characters' do
@@ -27,26 +26,23 @@ RSpec.describe User, type: :model do
   end
 
   context '.last_three_posts' do
-    let(:post1) { Post.create(author: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0) }
-    let(:post2) {Post.create(author: user, title: 'Hello', text: 'This is my second post', likes_counter: 0, comments_counter: 0)}
-    let(:post3) {Post.create(author: user, title: 'Hello', text: 'This is my third post', likes_counter: 0, comments_counter: 0)}
-    let(:post4) {Post.create(author: user, title: 'Hello', text: 'This is my fourth post', likes_counter: 0, comments_counter: 0)}
-    let(:post5) {Post.create(author: user, title: 'Hello', text: 'This is my fifth post', likes_counter: 0, comments_counter: 0)}
-
-    it 'returns empty array if user does not have any posts' do
-      posts_count = user.last_three_posts
-      expected = []
-      expect(posts_count).to eq(expected)
+    let(:post1) do
+      Post.create(author: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+    end
+    let(:post2) do
+      Post.create(author: user, title: 'Hello', text: 'This is my second post', likes_counter: 0, comments_counter: 0)
+    end
+    let(:post3) do
+      Post.create(author: user, title: 'Hello', text: 'This is my third post', likes_counter: 0, comments_counter: 0)
+    end
+    let(:post4) do
+      Post.create(author: user, title: 'Hello', text: 'This is my fourth post', likes_counter: 0, comments_counter: 0)
+    end
+    let(:post5) do
+      Post.create(author: user, title: 'Hello', text: 'This is my fifth post', likes_counter: 0, comments_counter: 0)
     end
 
-    it 'returns 1 if user has one post' do
-      post1.save!
-      posts_count = user.last_three_posts.length
-      expected = 1
-      expect(posts_count).to eq(expected)
-    end
-
-     it 'returns titles of last three posts if user has more three posts' do
+    it 'returns titles of last three posts if user has more three posts' do
       post2.save!
       post3.save!
       post4.save!
@@ -57,6 +53,14 @@ RSpec.describe User, type: :model do
       expected = [post5.title, post4.title, post3.title]
 
       expect(titles).to eq(expected)
+    end
+  end
+
+  context '.last_three_posts if user no posts' do
+    it 'returns empty array if user does not have any posts' do
+      posts_count = user.last_three_posts
+      expected = []
+      expect(posts_count).to eq(expected)
     end
   end
 end
