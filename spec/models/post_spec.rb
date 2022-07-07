4dde7bd1dc207acf4fa5d140c76260b6a59a1802
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 15:26:54 +0300

    Add test for Title must not be blank

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -23,6 +23,11 @@ RSpec.describe Post, type: :model do
       expect(post).to_not be_valid
     end
 
+    it 'is must not be blank' do
+      post.title = nil
+      expect(post).to_not be_valid
+    end
+
     it 'is valid with a name of more than 3 characters' do
       expect(post).to be_valid
     end
