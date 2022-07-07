722cc7be7edee7113f09bdbec7e839ec88669282
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-07 13:29:55 +0300

    Add associated of author and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true, length: { in: 3..10 }
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
