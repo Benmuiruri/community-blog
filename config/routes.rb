310e056e913c167142c029d0f731b3fab8bb3efd
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 13:51:40 +0300

    Update nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:index, :show], shallow: true do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  root to: 'users#index'
 end
