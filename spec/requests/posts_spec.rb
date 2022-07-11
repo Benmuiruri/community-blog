528c10c37c42c69f708c9387ec62c676265de6ea
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 17:52:59 +0300

    Add test for posts show action - test http code status, render template and response body

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include("Here is a list of posts for a given user")
     end
   end
+
+  describe 'GET /show' do
+    before(:example) { get user_post_path(1, 1) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "displays correct content in the view" do
+      expect(response.body).to include("Here is a User post")
+    end
+  end
 end
