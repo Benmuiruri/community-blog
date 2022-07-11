3d1e2036d344b331df8989361ad7eb5997a619bf
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 17:44:36 +0300

    Add test for users show action - test http code status, render template and response body

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include("Here is a list of all Users")
     end
   end
+
+  describe 'GET /show' do
+    before(:example) { get user_path(1) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "displays correct content in the view" do
+      expect(response.body).to include("Here is the profile of a user")
+    end
+  end
 end
