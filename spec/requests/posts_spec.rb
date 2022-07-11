df2b6d4521620a66f2a99564deb7fd45169dbe41
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 17:47:54 +0300

    Add test for posts show action - test http code status, render template and response body

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+ describe 'GET /index' do
+    before(:example) { get user_posts_path(1) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "displays correct content in the view" do
+      expect(response.body).to include("Here is a list of posts for a given user")
+    end
   end
 end
