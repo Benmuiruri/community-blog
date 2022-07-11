1a8480313d8a84599095333e62717e546ddada40
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-11 17:40:44 +0300

    Add test for users index view response body

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe 'Users', type: :request do
     it "renders 'index' template" do
       expect(response).to render_template(:index)
     end
+
+    it "displays correct content in the view" do
+      expect(response.body).to include("Here is a list of all Users")
+    end
   end
 end
