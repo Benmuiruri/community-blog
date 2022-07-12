1a9c339e34e22973ebce3a2c06472bb567e6493a
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-12 20:49:13 +0300

    Refactor post_spec file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,35 +1,41 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Posts', type: :request do
-#   describe 'GET /index' do
-#     before(:example) { get '/users/9/posts' }
+RSpec.describe 'Posts', type: :request do
+   let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Kenya.', posts_counter: 0) }
+    let(:post) do
+    Post.create(author: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+  end
+  
 
-#     it 'is a success' do
-#       expect(response).to have_http_status(:ok)
-#     end
+  describe 'GET /index' do
+    before(:example) { get user_posts_path(user.id) }
 
-#     it "renders 'index' template" do
-#       expect(response).to render_template(:index)
-#     end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
 
-#     it 'displays correct content in the view' do
-#       expect(response.body).to include('Tom')
-#     end
-#   end
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
 
-#   describe 'GET /show' do
-#     before(:example) { get '/users/9/posts/24' }
+    it 'displays correct content in the view' do
+      expect(response.body).to include('Teacher from Kenya')
+    end
+  end
 
-#     it 'is a success' do
-#       expect(response).to have_http_status(:ok)
-#     end
+  describe 'GET /show' do
+    before(:example) { get user_post_path(user.id, post) }
 
-#     it "renders 'show' template" do
-#       expect(response).to render_template(:show)
-#     end
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
 
-#     it 'displays correct content in the view' do
-#       expect(response.body).to include('Here is a User post')
-#     end
-#   end
-# end
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'displays correct content in the view' do
+      expect(response.body).to include('Hello')
+    end
+  end
+end
