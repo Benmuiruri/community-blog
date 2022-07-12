ffe8677f288bd3b840944a64a854efa1485efa9e
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-12 17:58:14 +0300

    Add comment partial in post index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.includes(:author).where(author: params[:user_id])
+    
   end
 
   def show
-
+    @post = Post.find(params[:id])
+    @last_five_comments = @post.last_five_comments
   end
 end
