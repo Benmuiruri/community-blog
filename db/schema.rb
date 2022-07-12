07b37c2864f51119cb5cc32d780b36b2496bb6a5
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-12 16:12:17 +0300

    Add migration to set default for user posts_counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_05_104717) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_130832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_05_104717) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
   end
 
   add_foreign_key "comments", "posts"
