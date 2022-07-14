1c163050954f9ee4de2f38de78050e05a789b299
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-14 09:57:00 +0300

    Add comments and likes routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :create, :new, :show]
+    resources :posts, only: [:index, :create, :new, :show] do
+      resources :comments, only: [:index, :create]
+      resources :likes, only: [:create]
+    end
   end
 
   root to: 'users#index'
