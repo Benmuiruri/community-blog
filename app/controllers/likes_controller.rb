f18b2d481959ee52ccaed1fce2384fb3a72b15ae
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-14 12:56:11 +0300

    Add delete route and action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,22 +1,25 @@
 class LikesController < ApplicationController
   def create
-    # Like.create(post: first_post, author: first_user)
+
     @like = Like.create(likes_params)
     @like.author = Current.user
-    @post = Post.find(params[:post_id])
-    @like.post = @post
+    post = Post.find(params[:post_id])
+    @like.post = post
 
      if @like.save
       flash[:notice] = 'Successfully liked post.'
-      redirect_to user_post_path(@post.author, @post)
+      redirect_to user_post_path(post.author, post)
     else
       flash[:notice] = 'Something went wrong, like not posted.'
-      redirect_to user_post_path(@post.author, @post)
+      redirect_to user_post_path(post.author, post)
     end
   end
 
   def destroy
-    
+    @like = Current.user.likes.find(params[:id])
+    @post = @like.post
+    @like.destroy
+    redirect_to user_post_path(post.author, post)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show] do
       resources :comments, only: [:index, :create]
-      resources :likes, only: [:create]
+      resources :likes, only: [:create, :destroy]
     end
   end
 
