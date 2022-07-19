aba4cd3109802807aae6809ac38672650c99d994
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-19 13:26:00 +0300

    Add test for presense of h1 on user show page

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -4,12 +4,15 @@ require 'helpers/create_test_models'
 RSpec.describe 'Users Show page', type: :feature do
   before(:all) do
     @username1 = 'Tom'
-    @user1= create_user(@username1)
+    @user1 = create_user(@username1)
+    visit user_path(id: @user1.id)
   end
 
   it 'Shows the static text' do
-    visit user_path(id: @user1.id)
     expect(page).to have_content('Tom\'s Most Recent Posts')
   end
 
+  it 'Shows the User\s name' do
+    expect(page).to have_content @username1 
+  end
 end
