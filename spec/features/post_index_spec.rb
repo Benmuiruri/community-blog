f92692b41b4da181827bf0ca8c9e94fe82ce53aa
maxthestranger <thestrangermax@gmail.com>
2022-07-19 13:10:31 +0300

    refactor and extract model creator methods to helper

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,40 +1,5 @@
 require 'rails_helper'
 
-def create_and_activate_user(name)
-  user = User.create!(
-    name:, bio: "Bio of #{name}", photo: "#{name}.jpg"
-  )
-  user.confirm
-  user.save
-  user
-end
-
-def create_posts_for_user(user, count: 1)
-  posts = []
-  count.times do |i|
-    posts << Post.create!(
-      user:,
-      title: "Post #{i}",
-      text: "This is body of post #{i}",
-      comments_counter: 0,
-      likes_counter: 0
-    )
-  end
-  posts
-end
-
-def create_comments_for_post_by_user(post, user, count: 1)
-  comments = []
-  count.times do |i|
-    comments << Comment.create!(
-      user:,
-      post:,
-      text: "Comment #{i}"
-    )
-  end
-  comments
-end
-
 describe 'Post Index' do
   before do
     @name = 'user'
