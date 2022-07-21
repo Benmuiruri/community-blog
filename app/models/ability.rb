652707bbdc0d1c4ae3437e65dd8151537d1088c8
Benmuiruri <ben.muiruri13@gmail.com>
2022-07-21 12:17:59 +0300

    Add authotization via cancan for delete comment action

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,15 @@ class Ability
 
   def initialize(user)
     can :read, Post
-
+    can :read, Comment
     return unless user.present?
 
     can %i[read create update destroy], Post, author: user
+    can %i[read create update destroy], Comment, author: user
 
     return unless user.role === 'admin'
 
     can :manage, Post
+    can :manage, Comment
   end
 end
